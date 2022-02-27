728f8adf28a278b09ddf935781ebb6d7ae77b02e
Jhoglas <jhoglassx@gmail.com>
2022-02-27 15:16:06 -0300

    implementation the list of rating user

--- app/src/main/java/com/ciandt/breweryees/ui/MainActivity.kt
+++ app/src/main/java/com/ciandt/breweryees/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.ciandt.breweryees.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ciandt.breweryees.R
@@ -37,6 +40,28 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        var menuInflater : MenuInflater = menuInflater
+
+        menuInflater.inflate(R.menu.main_menu,menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val res_id = item.itemId
+        if(res_id == R.id.action_rating){
+            Toast.makeText(this,"Rating Option",Toast.LENGTH_LONG).show()
+        }else if(res_id == R.id.action_favoritos){
+            Toast.makeText(this,"Favoritos Option",Toast.LENGTH_LONG).show()
+        }
+
+        return true
+    }
+
     fun viewSearch(search:String){
         val fragment = ResultFragment()
         val bundle = Bundle()
